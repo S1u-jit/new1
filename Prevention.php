4777d8cfc36df8852e52bb91cca53ed4ac04169a
S1u-jit <54575151+S1u-jit@users.noreply.github.com>
2020-04-17 09:47:33 +0530

    Add files via upload

---
+++ Prevention.php
@@ -0,0 +1,30 @@
+<div class="container" style="text-align: center; height: 100vh;">
+  <h1  style="font-family: 'Roboto', sans-serif;
+font-family: 'Girassol', cursive; ">Prevention</h1>
+
+
+<div class="row" style="justify-content:center;align-items: center;text-align: center; width: 90%!important;margin-left:2rem; border:2px solid red; height: 90vh!important;" >
+  <div class="col-sm-12 col-lg-4 col-md-4">
+    <div class="row">
+      <div class="col-sm-12 col-lg-6 col-md-6"><img src="image\mass.png" width="100px" height="100px"></div>
+      <div class="col-sm-12 col-lg-6 col-md-6">b</div>
+    </div>
+  </div>
+    <div class="col-sm-12 col-lg-4 col-md-4">
+    <div class="row">
+      <div class="col-sm-12 col-lg-6 col-md-6"><img src="image\protect-wash-hands.png" width="100px" height="100px"></div>
+      <div class="col-sm-12 col-lg-6 col-md-6">b</div>
+    </div>
+  </div>
+    <div class="col-sm-12 col-lg-4 col-md-4">
+    <div class="row">
+      <div class="col-sm-12 col-lg-6 col-md-6"><img src="image\distancing.png" width="100px" height="100px"></div>
+      <div class="col-sm-12 col-lg-6 col-md-6">b</div>
+    </div>
+  </div>
+</div><!--Row End-->
+
+</div><!---End of main dv--->
+
+
+
---
+++ Symptoms.php
@@ -0,0 +1,43 @@
+<div class="container" style="text-align: center;  height: 100vh;
+">
+  <h1  style="font-family: 'Roboto', sans-serif;
+font-family: 'Girassol', cursive; ">Symptoms</h1>
+
+
+<div class="row" style=" justify-content:center;align-items: center;text-align: center; width: 90%!important;margin-left:2rem; border:2px solid red; height: 90vh!important; " >
+  <div class="col-sm-12 col-lg-3 col-md-3">
+    <div class="row in">
+     <div class="col-sm-12 col-lg-9 col-md-9"><img src="image\fever.png" width="100px" height="100px">
+      </div>
+      <div class="col-sm-12 col-lg-3 col-md-3"><b>Fever</b></div>
+    </div>
+  </div>
+    <div class="col-sm-12 col-lg-3 col-md-3">
+    <div class="row in ">
+      <div class="col-sm-12 col-lg-9 col-md-9"><img src="image\cough.png" width="100px" height="100px"></div>
+      <div class="col-sm-12 col-lg-3 col-md-3"><b>Cough</b></div>
+    </div>
+  </div>
+    <div class="col-sm-12 col-lg-3 col-md-3">
+    <div class="row in ">
+      <div class="col-sm-12 col-lg-9 col-md-9"><img src="image\Weakness.png" width="100px" height="100px"></div>
+      <div class="col-sm-12 col-lg-3 col-md-3"><b>Weakness</b>
+    </div>
+  </div>
+</div>
+
+  <div class="col-sm-12 col-lg-12 col-md-12">
+    <div class="row in ">
+      <div class="col-sm-12 col-lg-12 col-md-12"><img src="image\breath.png" width="100px" height="100px"></div>
+      <div class="col-sm-12 col-lg-12 col-md-12"><b>Shortness Of breathing</b></div>
+    </div>
+  </div>
+</div>
+
+
+
+
+</div><!---End of main dv--->
+
+
+
